c5fa9a47189c96404c3756f440bcb98950398bb1
Antsch <antoine97410@hotmail.fr>
2024-04-26 10:27:39 +0200

    Update

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -8,14 +8,17 @@ class SettingsScreen extends StatefulWidget {
   _SettingsScreenState createState() => _SettingsScreenState();
 }
 
-enum Character { homme, femme, autre }
+enum Sector { public, prive }
 
 class _SettingsScreenState extends State<SettingsScreen> {
   // Initialisation des variables
   double sliderValue = 25.0;
-  Character? _character = Character.autre;
+  Sector? _sector = Sector.public;
   bool _isTermsAccepted = false;
   DateTime _selectedDate = DateTime.now();
+  String dropdownValue = 'Homme';
+  String? firstNameError;
+  String? lastNameError;
   final _firstNameController = TextEditingController();
   final _lastNameController = TextEditingController();
 
@@ -25,6 +28,14 @@ class _SettingsScreenState extends State<SettingsScreen> {
   ///
   /// Et affiche une boîte de dialogue en cas d'erreur
   void _validateForm() {
+    setState(() {
+      firstNameError = _firstNameController.text.isEmpty
+          ? 'Le prénom ne peut pas être vide'
+          : null;
+      lastNameError = _lastNameController.text.isEmpty
+          ? 'Le nom ne peut pas être vide'
+          : null;
+    });
     String? error;
     if (_firstNameController.text.isEmpty) {
       error = 'Le prénom ne peut pas être vide';
@@ -66,7 +77,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
         _lastNameController.clear();
         _isTermsAccepted = false;
         _selectedDate = DateTime.now();
-        _character = Character.autre;
+        _sector = Sector.public;
         sliderValue = 25.0;
       });
 
@@ -97,10 +108,10 @@ class _SettingsScreenState extends State<SettingsScreen> {
   /// Met à jour la date sélectionnée
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
-        context: context,
-        initialDate: _selectedDate,
-        firstDate: DateTime(1900, 1),
-        lastDate: DateTime(2100),
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(1900, 1),
+      lastDate: DateTime(2100),
     );
     if (picked != null) {
       setState(() {
@@ -124,146 +135,183 @@ class _SettingsScreenState extends State<SettingsScreen> {
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 20.0),
         color: Colors.white,
-        child: Center(
-          // Liste des champs du formulaire organisés en colonne
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-               Padding(
-                padding: const EdgeInsets.all(15.0),
-                // Champ de texte pour le prénom avec décoration
-                child: TextField(
-                  controller: _firstNameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Prénom',
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.indigo, width: 2.0),
+        child: SingleChildScrollView(
+          child: Center(
+            // Liste des champs du formulaire organisés en colonne
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    controller: _firstNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Prénom',
+                      errorText: firstNameError,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: firstNameError == null
+                                ? Colors.indigo
+                                : Colors.red,
+                            width: 2.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                // Champ de texte pour le nom avec décoration
-                child: TextField(
-                  controller: _lastNameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Nom',
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.indigo, width: 2.0),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: TextField(
+                    controller: _lastNameController,
+                    decoration: InputDecoration(
+                      labelText: 'Nom',
+                      errorText: lastNameError,
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: lastNameError == null
+                                ? Colors.indigo
+                                : Colors.red,
+                            width: 2.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Text(
-                'Age : ${sliderValue.round()}',
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                // Curseur pour l'âge
-                child: Slider(
-                  value: sliderValue,
-                  min: 18,
-                  max: 40,
-                  divisions: 22,
-                  label: sliderValue.round().toString(),
-                  activeColor: Colors.indigo,
-                  inactiveColor: Colors.indigo.withOpacity(0.2),
-                  onChanged: (double newValue) {
+                const SizedBox(height: 20),
+                Text(
+                  'Age : ${sliderValue.round()}',
+                  style: const TextStyle(
+                      fontSize: 15.0, fontWeight: FontWeight.bold),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  // Curseur pour l'âge
+                  child: Slider(
+                    value: sliderValue,
+                    min: 18,
+                    max: 40,
+                    divisions: 22,
+                    label: sliderValue.round().toString(),
+                    activeColor: Colors.indigo,
+                    inactiveColor: Colors.indigo.withOpacity(0.2),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        sliderValue = newValue;
+                      });
+                    },
+                  ),
+                ),
+                const Text(
+                  'Vous êtes : ',
+                  style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                ),
+                DropdownButton<String>(
+                  value: dropdownValue,
+                  elevation: 16,
+                  style: const TextStyle(color: Colors.indigo),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.indigo,
+                  ),
+                  onChanged: (String? newValue) {
                     setState(() {
-                      sliderValue = newValue;
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: <String>['Homme', 'Femme', 'Autre']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(height: 20),
+                // Liste des boutons radio pour le genre
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Dans quel secteur travaillez-vous : ',
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.bold),
+                    ),
+                    RadioListTile<Sector>(
+                      title: const Text(
+                        'Public',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      activeColor: Colors.indigo,
+                      value: Sector.public,
+                      groupValue: _sector,
+                      onChanged: (Sector? value) {
+                        setState(() {
+                          _sector = value;
+                        });
+                      },
+                    ),
+                    RadioListTile<Sector>(
+                      title: const Text(
+                        'Privé',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      activeColor: Colors.indigo,
+                      value: Sector.prive,
+                      groupValue: _sector,
+                      onChanged: (Sector? value) {
+                        setState(() {
+                          _sector = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                // Bouton pour sélectionner la date
+                ElevatedButton(
+                  onPressed: () => _selectDate(context),
+                  child: Text(
+                      'Capchat : Sélectionner la date du jour : '
+                      '${DateFormat('dd/MM/yyyy').format(_selectedDate)}',
+                      style: const TextStyle(color: Colors.indigo)),
+                ),
+                const SizedBox(height: 20),
+                // Checkbox pour accepter les conditions d'utilisation
+                CheckboxListTile(
+                  title: const Text(
+                    'J\'accepte les Conditions Générales d\'utilisation',
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+                  value: _isTermsAccepted,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _isTermsAccepted = value ?? false;
                     });
                   },
                 ),
-              ),
-              // Liste des boutons radio pour le genre
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Vous êtes : ',
-                    style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+                // Bouton de validation du formulaire
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.indigo),
                   ),
-                  RadioListTile<Character>(
-                    title: const Text(
-                      'Homme',
-                      style: TextStyle(fontSize: 14.0),
-                    ),
-                    activeColor: Colors.indigo,
-                    value: Character.homme,
-                    groupValue: _character,
-                    onChanged: (Character? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
+                  onPressed: _validateForm,
+                  child: const Text(
+                    'Valider le formulaire',
+                    style: TextStyle(color: Colors.white),
                   ),
-                  RadioListTile<Character>(
-                    title: const Text(
-                      'Femme',
-                      style: TextStyle(fontSize: 14.0),
-                    ),
-                    activeColor: Colors.indigo,
-                    value: Character.femme,
-                    groupValue: _character,
-                    onChanged: (Character? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
-                  RadioListTile<Character>(
-                    title: const Text(
-                      'Autre',
-                      style: TextStyle(fontSize: 14.0),
-                    ),
-                    activeColor: Colors.indigo,
-                    value: Character.autre,
-                    groupValue: _character,
-                    onChanged: (Character? value) {
-                      setState(() {
-                        _character = value;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              // Bouton pour sélectionner la date
-              ElevatedButton(
-                onPressed: () => _selectDate(context),
-                child: Text('Capchat : Sélectionner la date du jour : ${DateFormat('dd/MM/yyyy').format(_selectedDate)}'),
-              ),
-              // ...
-              // Checkbox pour accepter les conditions d'utilisation
-              CheckboxListTile(
-                title: const Text(
-                  'J\'accepte les Conditions Générales d\'utilisation',
-                  style: TextStyle(fontSize: 14.0),
                 ),
-                value: _isTermsAccepted,
-                onChanged: (bool? value) {
-                  setState(() {
-                    _isTermsAccepted = value ?? false;
-                  });
-                },
-              ),
-              // Bouton de validation du formulaire
-              ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.indigo),
-                ),
-                onPressed: _validateForm,
-                child: const Text(
-                  'Valider le formulaire',
-                  style: TextStyle(color: Colors.white),
-                ),
-              )
-            ],
+                const SizedBox(height: 20),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    super.dispose();
+  }
 }
-
-
