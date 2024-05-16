6a94bcad11fc4ddcb3b8666a83282ec6de56f6e2
Antsch <antoine.schwager@student.junia.com>
2024-05-16 12:12:05 +0200

    Add login/logout feature in the MyScaffold widget

--- lib/widgets/myscaffold.dart
+++ lib/widgets/myscaffold.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/auth_provider.dart';
 
 class MyScaffold extends StatelessWidget {
   final Widget body;
   final String name;
 
-  const MyScaffold(
-      {super.key,
-      required this.body,
-      required this.name});
+  const MyScaffold({super.key, required this.body, required this.name});
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(name, style: const TextStyle(color: Colors.white)),
@@ -24,6 +26,27 @@ class MyScaffold extends StatelessWidget {
                 context,
                 '/register',
               );
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              authProvider.isLoggedIn ? Icons.logout : Icons.login,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              if (authProvider.isLoggedIn) {
+                authProvider.logout();
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  '/login',
+                  (Route<dynamic> route) => false,
+                );
+              } else {
+                Navigator.pushNamed(
+                  context,
+                  '/login',
+                );
+              }
             },
           ),
         ],
