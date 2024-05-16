6d9b8590c8dc4a75ea2660d4bd8f2ca7d97287b1
Antsch <antoine.schwager@student.junia.com>
2024-05-16 10:21:37 +0200

    [refactor] Chapitre 10 - Inscription API pour convenir à l'IDE

--- lib/widgets/myscaffold.dart
+++ lib/widgets/myscaffold.dart
@@ -3,25 +3,22 @@ import 'package:flutter/material.dart';
 class MyScaffold extends StatelessWidget {
   final Widget body;
   final String name;
-  final List<Widget> actions;
 
   const MyScaffold(
-      {Key? key,
+      {super.key,
       required this.body,
-      required this.name,
-      this.actions = const []})
-      : super(key: key);
+      required this.name});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.name, style: TextStyle(color: Colors.white)),
+        title: Text(name, style: const TextStyle(color: Colors.white)),
         elevation: 10.0,
         centerTitle: true,
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.app_registration, color: Colors.white),
+            icon: const Icon(Icons.app_registration, color: Colors.white),
             onPressed: () {
               Navigator.pushNamed(
                 context,
@@ -33,9 +30,9 @@ class MyScaffold extends StatelessWidget {
       ),
       backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
       body: body,
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: const FloatingActionButton(
         onPressed: null,
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
   }
