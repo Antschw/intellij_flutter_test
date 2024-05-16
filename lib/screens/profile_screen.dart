5ec974e13ed5252f48700fab40ecdc72390880fa
Antsch <antoine.schwager@student.junia.com>
2024-05-16 14:45:35 +0200

    Added user's first and last name to Profile Screen

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intellij_flutter_test/utils/secure_storage.dart';
+import 'package:intellij_flutter_test/api/user.dart';
+import 'dart:convert';
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   _ProfileScreenState createState() => _ProfileScreenState();
@@ -11,19 +13,43 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   SecureStorage storage = SecureStorage();
   String _email = '';
+  String _firstName = '';
+  String _lastName = '';
 
   @override
   void initState() {
     super.initState();
-    getEmail();
+    getEmailAndName();
   }
 
-  getEmail() async {
+  getEmailAndName() async {
     final credentials = await storage.readCredentials();
     if (mounted) {
       setState(() {
         _email = credentials['email'] ?? '';
       });
+    }
+    final responseData = await getUserData();
+    if (responseData != null && mounted) {
+      setState(() {
+        _firstName = responseData['user']['firstName'] ?? '';
+        _lastName = responseData['user']['lastName'] ?? '';
+      });
+    }
+  }
+
+  Future<Map<String, dynamic>?> getUserData() async {
+    final credentials = await storage.readCredentials();
+    try {
+      final response = await login(credentials['email'] ?? '', credentials['password'] ?? '');
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to get user data: ${response.reasonPhrase}');
+      }
+    } catch (e) {
+      print("Exception while fetching user data: $e");
+      return null;
     }
   }
 
@@ -40,6 +66,14 @@ class _ProfileScreenState extends State<ProfileScreen> {
           children: <Widget>[
             Text(
               'Email: $_email',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              'First Name: $_firstName',
+              style: TextStyle(fontSize: 24),
+            ),
+            Text(
+              'Last Name: $_lastName',
               style: TextStyle(fontSize: 24),
             ),
           ],
